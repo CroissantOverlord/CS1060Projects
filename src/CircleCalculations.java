522f0a9c2a2c46b02466877f002c58120423cc39
CroissantOverlord <143460291+CroissantOverlord@users.noreply.github.com>
2023-09-19 22:46:51 -0600

    Update CircleCalculations.java

--- src/CircleCalculations.java
+++ src/CircleCalculations.java
@@ -5,22 +5,21 @@ public class CircleCalculations {
 
         System.out.println("Enter circle radius.");//prints out text saying Enter circle radius
         double radius = sc.nextDouble();//puts the number they entered into the variable radius
-        diameter(radius);//calls the function diameter
-        area(radius);//calls the function area
-        circumference(radius);//calls the function circumference
-    }
-    public static void diameter(double radius) {//makes a new function called diameter and pulls the variable radius for usage
-
-        double diameter = (2 * radius);// takes the radius and calculates the diameter by multiplying it by 2 and puts the calcutaion into the variable diameter
-        System.out.println("Diameter: " + diameter);// prints out the diameter
-    }
-    public static void area(double radius) {
+        double diameter = (2 * radius);//puts the calculation to convert radius into diameter into the variable diameter
         double area = (Math.PI * (radius * radius));
-        System.out.println("Area: " + area);
-    }
-    public static void circumference(double radius) {
         double circ = (2 * Math.PI * radius);
+        System.out.println("Diameter: " + diameter);// returns the calculated diameter from the diameter return function
+        System.out.println("Area: " + area);
         System.out.println("Circumference: " + circ);
+    }
+    public static double diameter(double radius) {//makes a new function called diameter and pulls the variable radius for usage
+        return (2 * radius);
+    }
+    public static double area(double radius) {
+        return (Math.PI * (radius * radius));
+    }
+    public static double circumference(double radius) {
+        return (2 * Math.PI * radius);
     }
 
 }
