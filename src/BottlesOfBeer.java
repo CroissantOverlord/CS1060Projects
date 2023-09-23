7ceccb8c337fc42f697712b455fbe621e0e59169
CroissantOverlord <143460291+CroissantOverlord@users.noreply.github.com>
2023-09-22 18:06:11 -0600

    Update BottlesOfBeer.java

--- src/BottlesOfBeer.java
+++ src/BottlesOfBeer.java
@@ -3,8 +3,18 @@ public class BottlesOfBeer {
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter the starting number of bottles.");
-        
+        System.out.println("Please enter the starting number of bottles: ");
+        int numBottles = sc.nextInt();
+        String bottles = ("bottles");
+
+        for (int x = numBottles; x > 0; x--) {
+            for (string y = bottles; y >;   )
+            System.out.println( x + " bottles of beer on the wall, " +  x + " bottles of beer.\n" +
+                    " Take one down and pass it around," + (x-1)  + " bottles of beer on the wall.");
+        }
+
+        System.out.println("\nYou need help");
+
 
     }
 }
