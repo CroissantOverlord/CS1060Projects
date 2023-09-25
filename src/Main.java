a21cfb746851495e85543c0b696cddb6e7b649e2
CroissantOverlord <143460291+CroissantOverlord@users.noreply.github.com>
2023-09-25 17:48:33 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,18 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        String message1 = "This is";
-        String message2 = " a message";
-        System.out.print(message1);
-        System.out.print(message2 + "\n");
-        System.out.print(" Hams" + "\n");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please type ur name");
+        String nameInput = sc.next();
 
+        if (nameInput.equals("Daylon")){
+            System.out.println("Your a chad");
 
-        String carDetails = "a 2001 " +
-                "Toyota Camry" + " with 200k Miles";
+        }
+        else if (nameInput.equals("SMELLS")) {
+            System.out.println("You suck and smell");
+        }
 
-        System.out.print("Your car details are ");
-        System.out.print(carDetails);
 
 
 
