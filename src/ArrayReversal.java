fe0545c34d2515a15be61bd0451decac72a712e2
CroissantOverlord <143460291+CroissantOverlord@users.noreply.github.com>
2023-10-11 22:57:09 -0600

    Update ArrayReversal.java

--- src/ArrayReversal.java
+++ src/ArrayReversal.java
@@ -1,33 +1,43 @@
 import java.util.Scanner;
+
 public class ArrayReversal {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        getNumber(sc);
-
-
-
-
-
-    }
-
-    public static void getNumber(Scanner sc){
-        int num;
         int times = 0;
-        while(times <= 100) {
-            System.out.println("Enter a value (-1 to stop program)");
-            num = sc.nextInt();
-            if (num < 0){
-                System.out.println("Error enter a positive number");
-                continue;
+
+        int[] inputArray = new int[100];
+        int arraySize = 0;
+
+        while (times < 100) {
+            int num = getNumber(sc);
+            if (num < 0) {
+                break;
             }
-            break;
+            inputArray[arraySize] = num;
+            arraySize++;
+            times++;
         }
-        
+        reverseArray(inputArray, arraySize);
+        printArray(inputArray, arraySize);
     }
 
+    public static int getNumber(Scanner sc) {
+        System.out.println("Enter a value (-1 to stop program)");
+        return sc.nextInt();
+    }
 
+    public static void reverseArray(int[] array, int size) {
+        int[] reversedArray = new int[size];
+        for (int i = 0; i < size; i++) {
+            reversedArray[i] = array[size - 1 - i];
+        }
 
-
+        System.arraycopy(reversedArray, 0, array, 0, size);
+    }
+    public static void printArray(int[] array, int size) {
+        System.out.print("Array contents: ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
     }
 }
