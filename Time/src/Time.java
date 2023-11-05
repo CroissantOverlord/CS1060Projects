00d27fd525a7e19b5c94465964f7fa127bcaa50b
CroissantOverlord <143460291+CroissantOverlord@users.noreply.github.com>
2023-11-05 13:17:21 -0700

    ti

---
+++ Time/src/Time.java
@@ -0,0 +1,18 @@
+public class Time {
+
+    final int hours;
+
+    final int minutes;
+    final int seconds;
+
+
+
+    public Time (int hours, int minutes, int seconds){
+
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+
+
+    }
+}
